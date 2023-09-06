62fd3ae9cb38b522417801c7d91156877462fd7f
jovsz <jovannyfuentesm@gmail.com>
2023-09-06 16:35:45 -0700

    Signup update

--- task/urls.py
+++ task/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import list_tasks, create_task, update_task, delete_task, put_task
+from .views import list_tasks, create_task, update_task, delete_task, put_task,signup, home
 
 
 urlpatterns = [
-    path('', list_tasks),
-    path('new/', create_task, name='create_task'),
-    path('update_task/<int:id>', update_task, name='update_task'),
-    path('put_task/<int:id>/', put_task, name='put_task'),
-    path('delete_task/<int:id>/', delete_task, name='delete_task')
+    
+    path('', home),
+    path('signup/', signup),
+    path('main/', list_tasks),
+    path('main/new/', create_task, name='create_task'),
+    path('main/update_task/<int:id>', update_task, name='update_task'),
+    path('main/put_task/<int:id>/', put_task, name='put_task'),
+    path('/main/delete_task/<int:id>/', delete_task, name='delete_task')
+    
 ]
--- task/views.py
+++ task/views.py
@@ -1,47 +1,85 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
 from .models import Task
 
+
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+def signup(request):
+    errorLength = True
+    print("obteniendo datos", request.POST)
+    if request.method == "GET":
+        print("enviando formulario")
+        return render(request, "signup.html", {"form": UserCreationForm, "error": False})
+    elif request.method == "POST":
+        if request.POST["password"] != request.POST["repassword"]:
+            return render(
+                request, "signup.html", {"form": UserCreationForm, "errorRe": True}
+            )
+
+        if len(request.POST["password"]) < 8:
+            return render(
+                request, "signup.html", {"form": UserCreationForm, "error": errorLength}
+            )
+
+        errorLength = False
+
+        return render(
+            request, "signup.html", {"form": UserCreationForm, "error": errorLength}
+        )
+
+
 def list_tasks(request):
-  tasks = Task.objects.all()
-  return render(request, 'list_tasks.html', {"tasks": tasks})
+    tasks = Task.objects.all()
+    return render(request, "list_tasks.html", {"tasks": tasks})
+
 
 def create_task(request):
-  print(request.POST)
-  done = False
-  
-  if request.POST['done']:
-    done = True
-  else:
     done = False
-    
-  task = Task(title=request.POST['title'], description=request.POST['description'], done= done)
-  task.save()
-  return redirect('/tasks/')
+
+    try:
+        if request.POST["done"]:
+            done = True
+        else:
+            done = False
+    except:
+        done = False
+
+    task = Task(
+        title=request.POST["title"], description=request.POST["description"], done=done
+    )
+    task.save()
+    return redirect("/main/")
+
 
 def update_task(request, id):
-  print(request.POST)
-  # print(id)
-  task = get_object_or_404(Task, id=id)
-  tasks = Task.objects.all()
-  
-  return render(request,'update_task.html', {"select":task, "tasks": tasks})
+    task = get_object_or_404(Task, id=id)
+    tasks = Task.objects.all()
 
-def put_task(request, id): 
-  print(request.POST['done'])
-  done = False
-  if done == 'on':
-    done = True
-  else:
-    done == False
-    
-  Task.objects.filter(id = id).update(title=request.POST['title'], description=request.POST['description'], done= done)
-  return redirect('/tasks/')
+    return render(request, "update_task.html", {"select": task, "tasks": tasks})
 
+
+def put_task(request, id):
+    done = False
+    try:
+        if request.POST["done"]:
+            done = True
+        else:
+            done == False
+    except:
+        done = False
+
+    Task.objects.filter(id=id).update(
+        title=request.POST["title"], description=request.POST["description"], done=done
+    )
+    return redirect("/main/")
 
 
 def delete_task(request, id):
-  print(id)
-  task = Task.objects.get(id = id)
-  task.delete()
-  return redirect('/tasks/')
+    print(id)
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect("/main/")
